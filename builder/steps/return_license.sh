2ef785ceac677ca22de3e6323cc106a7199cc080
Webber <webber@takken.io>
2020-01-08 00:52:58 +0100

    Abstract away steps and remove output variables

---
+++ builder/steps/return_license.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#
+# PROFESSIONAL (SERIAL) LICENSE MODE
+#
+# This will return the license that is currently in use.
+#
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
+  /opt/Unity/Editor/Unity \
+    -batchmode \
+    -nographics \
+    -logFile /dev/stdout \
+    -quit \
+    -returnlicense
