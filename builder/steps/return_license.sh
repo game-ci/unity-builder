67b310bf60465531a4d5befc53131fe597eb8cdc
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2020-01-12 19:17:07 +0000

    fix(license): only return the license in pro mode

--- builder/steps/return_license.sh
+++ builder/steps/return_license.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-#
-# PROFESSIONAL (SERIAL) LICENSE MODE
-#
-# This will return the license that is currently in use.
-#
-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
-  /opt/Unity/Editor/Unity \
-    -batchmode \
-    -nographics \
-    -logFile /dev/stdout \
-    -quit \
-    -returnlicense
+if [[ -n "$UNITY_SERIAL" ]]; then
+  #
+  # PROFESSIONAL (SERIAL) LICENSE MODE
+  #
+  # This will return the license that is currently in use.
+  #
+  xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
+    /opt/Unity/Editor/Unity \
+      -batchmode \
+      -nographics \
+      -logFile /dev/stdout \
+      -quit \
+      -returnlicense
+fi
