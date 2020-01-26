846d5043bb17173073c6b5c3342b217e39943ecb
Webber <webber@takken.io>
2020-01-26 01:01:53 +0100

    Minor cleanup

---
+++ src/model/index.js
@@ -0,0 +1,8 @@
+import Action from './action';
+import BuildParameters from './build-parameters';
+import Docker from './docker';
+import Input from './input';
+import ImageTag from './image-tag';
+import Platform from './platform';
+
+export { Action, BuildParameters, Docker, Input, ImageTag, Platform };
