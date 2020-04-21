31b8b07adea8e7c9bfa1043b6c84488632007699
Webber <webber@takken.io>
2020-04-21 23:03:12 +0200

    Add semantic versioning logic

---
+++ action/default-build-script/Assets/Editor/Versioning/GitException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnityBuilderAction.Versioning
+{
+  public class GitException : InvalidOperationException
+  {
+    public readonly int code;
+
+    public GitException(int code, string errors) : base(errors)
+    {
+      this.code = code;
+    }
+  }
+}
