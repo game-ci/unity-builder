615f4ee4e2af57f4b2df24686b1ac09663ff73be
Webber <webber@takken.io>
2020-09-27 21:19:42 +0200

    Add intructions for debugging

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -9,6 +9,23 @@ source /steps/build.sh
 source /steps/return_license.sh
 
 #
+# Instructions for debugging
+#
+
+if [[ $BUILD_EXIT_CODE -gt 0 ]]; then
+echo ""
+echo "###########################"
+echo "#         Failure         #"
+echo "###########################"
+echo ""
+echo "Please note that the exit code is not very descriptive."
+echo "Most likely it will not help you solve the issue."
+echo ""
+echo "To find the reason for failure: please search for errors in the log above."
+echo ""
+fi;
+
+#
 # Exit with code from the build step.
 #
 
