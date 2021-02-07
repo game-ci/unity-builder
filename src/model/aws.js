15c588c6c5b5a70bc280fa209a8bb9934f89fd6d
Frostebite <jas.f.ukcmti@gmail.com>
2021-02-07 19:07:37 +0000

    Decode kinesis data

--- src/model/aws.js
+++ src/model/aws.js
@@ -145,7 +145,7 @@ class AWS {
       if (records.Records.length > 0) {
         for (let index = 0; index < records.Records.length; index++) {
           const record = records.Records[index].Data;
-          core.info(Buffer.from(record.toString('base64')).toString('ascii'));
+          core.info(Buffer.from(record.toString(), 'base64').toString('ascii'));
         }
       }
       await new Promise((resolve) => setTimeout(resolve, 3000));
