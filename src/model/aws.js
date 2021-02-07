123330b31f86bcd310e7a7feda18a07543606757
Frostebite <jas.f.ukcmti@gmail.com>
2021-02-07 21:30:47 +0000

    Decode kinesis data

--- src/model/aws.js
+++ src/model/aws.js
@@ -144,7 +144,7 @@ class AWS {
       iterator = records.NextShardIterator;
       if (records.Records.length > 0) {
         for (let index = 0; index < records.Records.length; index++) {
-          core.info(zlib.deflateSync(records.Records[index].Data).toString('utf8'));
+          core.info(records.Records[index].Data);
         }
       }
       await new Promise((resolve) => setTimeout(resolve, 3000));
