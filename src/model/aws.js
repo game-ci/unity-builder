2a5f42b8fa2883361b76ab414ffb4024055e9ea8
Frostebite <jas.f.ukcmti@gmail.com>
2021-02-07 17:31:11 +0000

    Process every record from kinesis as b64 buffer

--- src/model/aws.js
+++ src/model/aws.js
@@ -143,7 +143,7 @@ class AWS {
       if (records.Records.length > 0) {
         for (let index = 0; index < records.Records.length; index++) {
           const element = records.Records[index];
-          core.info(element.Data.toString('utf8'));
+          core.info(element.Data.toString());
         }
       }
     }
