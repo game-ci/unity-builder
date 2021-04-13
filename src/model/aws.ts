8b235751275ef838b3c66368bf37d01f86f7bd77
mdugdale <mark.dugdale@bossastudios.com>
2021-04-14 00:42:58 +0100

    cleanup

--- src/model/aws.ts
+++ src/model/aws.ts
@@ -49,7 +49,6 @@ class AWS {
           ls
           echo ' '
           libDir="/${efsDirectoryName}/${buildUid}/${repositoryDirectoryName}/${buildParameters.projectPath}/Library"
-          if library directory doesn't exist create it, 
           if [ -d "$libDir"  ]; then
             echo "Library folder already present, make sure you setup .gitignore correctly (cleaning out Library folder for this build)!"
             rm -r $libDir
