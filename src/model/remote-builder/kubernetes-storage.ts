b9cf74b2e64fc95b9b5bf6a225d93daf31d8f306
Frostebite <jas.f.ukcmti@gmail.com>
2021-06-19 02:40:34 +0100

    Fix: log all pvc info

--- src/model/remote-builder/kubernetes-storage.ts
+++ src/model/remote-builder/kubernetes-storage.ts
@@ -2,13 +2,13 @@ import waitUntil from 'async-wait-until';
 import * as core from '@actions/core';
 import * as k8s from '@kubernetes/client-node';
 import BuildParameters from '../build-parameters';
-import { strict as assert } from 'assert';
 
 class KubernetesStorage {
   public static async getPVCPhase(kubeClient: k8s.CoreV1Api, name: string, namespace: string) {
     return (await kubeClient.readNamespacedPersistentVolumeClaim(name, namespace)).body.status?.phase;
   }
   public static async watchUntilPVCNotPending(kubeClient: k8s.CoreV1Api, name: string, namespace: string) {
+    core.info(`watch Until PVC Not Pending ${name} ${namespace}`);
     await waitUntil(async () => (await this.getPVCPhase(kubeClient, name, namespace)) !== 'Pending', {
       timeout: 500000,
       intervalBetweenAttempts: 15000,
@@ -42,8 +42,7 @@ class KubernetesStorage {
       },
     };
     const result = await kubeClient.createNamespacedPersistentVolumeClaim(namespace, pvc);
-    core.info(`Persistent Volume Claim ${result.body.metadata?.name} created`);
-    assert.strictEqual(result.body.metadata?.name, pvcName);
+    core.info(`Persistent Volume Claim ${result.body.metadata?.name} ${pvcName} created`);
   }
 }
 
