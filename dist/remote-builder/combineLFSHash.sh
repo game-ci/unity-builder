69bb893d4e714974e8549a4357c43360c423dc23
Frostebite <jas.f.ukcmti@gmail.com>
2021-07-12 23:25:50 +0100

    combine lfs hash file logging improvements

--- dist/remote-builder/combineLFSHash.sh
+++ dist/remote-builder/combineLFSHash.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-echo "Combining LFS hash files into one hash, this is used as the cache key:"
+echo "Combining LFS hash files into one hash, this can be used to cache LFS files"
 git lfs ls-files -l | cut -d' ' -f1 | sort > $1/lfsSum.txt
-ls -a
+echo ''
+echo 'combined file:'
 cat $1/lfsSum.txt
+echo ''
