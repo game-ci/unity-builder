d9bb6a323a8cf4fe91c909a1f8a86cd74a4d4442
Frostebite <jas.f.ukcmti@gmail.com>
2021-07-13 00:01:51 +0100

    Re-enable git lfs

--- dist/remote-builder/cloneNoLFS.sh
+++ dist/remote-builder/cloneNoLFS.sh
@@ -6,7 +6,7 @@ githubSha=$3
 
 cd $repoPathFull
 
-echo ''
+echo ' '
 echo "Cloning the repository being built:"
 # DISABLE LFS
 git config --global filter.lfs.smudge "git-lfs smudge --skip -- %f"
@@ -17,5 +17,9 @@ git remote add origin $cloneUrl
 # Get remote version
 git fetch origin
 git reset --hard $githubSha
+# ENABLE LFS
+git config --global filter.lfs.smudge "git-lfs smudge -- %f"
+git config --global filter.lfs.process "git-lfs filter-process"
+# List git lfs files
 git lfs ls-files --all
-echo ''
+echo ' '
