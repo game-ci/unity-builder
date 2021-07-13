462bb3cb19b2ac387ff272606bb603dbc4962978
Frostebite <jas.f.ukcmti@gmail.com>
2021-07-13 01:12:49 +0100

    late install git lfs

--- dist/remote-builder/cloneNoLFS.sh
+++ dist/remote-builder/cloneNoLFS.sh
@@ -8,16 +8,9 @@ cd $repoPathFull
 
 echo ' '
 echo "Cloning the repository being built:"
-# DISABLE LFS
-export GIT_LFS_SKIP_SMUDGE=1
-# Init new repo and setup origin
-git init
-git remote add origin $cloneUrl
-# Get remote version
-git fetch origin
-git reset --hard $githubSha
-# ENABLE LFS
-export GIT_LFS_SKIP_SMUDGE=0
+git clone $cloneUrl $githubSha
+
+apk add git-lfs
 
 tree
 echo ' '
@@ -26,6 +19,6 @@ git lfs ls-files --all
 echo ' '
 git lfs ls-files -l | cut -d ' ' -f1 | sort
 echo ' '
-git lfs ls-files -l | cut -d ' ' -f1 | sort > $1/.lfs-assets-id
+git lfs ls-files -l | cut -d ' ' -f1 | sort > .lfs-assets-id
 
 echo ' '
