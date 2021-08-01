beabee0e7f4262257df710aee0458c0f25807c0c
Frostebite <jas.f.ukcmti@gmail.com>
2021-08-01 01:01:54 +0100

    git lfs pull

--- dist/remote-builder/cloneNoLFS.sh
+++ dist/remote-builder/cloneNoLFS.sh
@@ -9,7 +9,8 @@ cd $repoPathFull
 
 echo ' '
 echo "Cloning the repository being built:"
-git clone --filter=blob:none --no-checkout $cloneUrl $repoPathFull
+git lfs install --skip-smudge
+git clone $cloneUrl $repoPathFull
 git checkout $githubSha
 echo "Checked out $githubSha"
 
