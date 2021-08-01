2a1d36b928f0fe8987bf5cc7c4e9359c2fc82824
Frostebite <jas.f.ukcmti@gmail.com>
2021-08-02 00:38:30 +0100

    fix

--- src/model/remote-builder/remote-builder.ts
+++ src/model/remote-builder/remote-builder.ts
@@ -5,59 +5,47 @@ import RemoteBuilderNamespace from './remote-builder-namespace';
 import RemoteBuilderSecret from './remote-builder-secret';
 import { RemoteBuilderProviderInterface } from './remote-builder-provider-interface';
 import Kubernetes from './kubernetes-build-platform';
+import RemoteBuilderEnvironmentVariable from './remote-builder-environment-variable';
 const repositoryFolder = 'repo';
 const buildVolumeFolder = 'data';
 const cacheFolder = 'cache';
 const cacheFolderFull = `/${buildVolumeFolder}/${cacheFolder}`;
 
 class RemoteBuilder {
-  static SteamDeploy: boolean = false;
+  private static buildId: string;
+  private static branchName: string;
+  private static buildParams: BuildParameters;
+  static RemoteBuilderProviderPlatform: RemoteBuilderProviderInterface;
+  private static defaultSecrets: RemoteBuilderSecret[];
+  static SteamDeploy: boolean = process.env.STEAM_DEPLOY !== undefined || false;
   private static buildPathFull: string;
   private static builderPathFull: string;
   private static steamPathFull: string;
   private static repoPathFull: string;
   private static projectPathFull: string;
   private static libraryFolderFull: string;
-  private static buildId: string;
-  private static buildParams: BuildParameters;
-  private static defaultSecrets: RemoteBuilderSecret[];
-  static RemoteBuilderProviderPlatform: RemoteBuilderProviderInterface;
+  private static readonly defaultGitShaEnvironmentVariable = [
+    {
+      name: 'GITHUB_SHA',
+      value: process.env.GITHUB_SHA || '',
+    },
+  ];
+
   static async build(buildParameters: BuildParameters, baseImage) {
-    const runNumber = process.env.GITHUB_RUN_NUMBER;
-    if (!runNumber || runNumber === '') {
-      throw new Error('no run number found, exiting');
-    }
+    RemoteBuilder.buildId = RemoteBuilderNamespace.generateBuildName(
+      RemoteBuilder.readRunNumber(),
+      buildParameters.platform,
+    );
     RemoteBuilder.buildParams = buildParameters;
-    RemoteBuilder.buildId = RemoteBuilderNamespace.generateBuildName(runNumber, buildParameters.platform);
-    const defaultBranchName =
-      process.env.GITHUB_REF?.split('/')
-        .filter((x) => {
-          x = x[0].toUpperCase() + x.slice(1);
-          return x;
-        })
-        .join('') || '';
-    const branchName =
-      process.env.REMOTE_BUILDER_CACHE !== undefined ? process.env.REMOTE_BUILDER_CACHE : defaultBranchName;
-    this.SteamDeploy = process.env.STEAM_DEPLOY !== undefined || false;
-    const token: string = this.buildParams.githubToken;
-    RemoteBuilder.newMethod(token);
+    RemoteBuilder.readBranchNameSetting();
+    RemoteBuilder.setupDefaultSecrets();
     try {
-      switch (this.buildParams.remoteBuildCluster) {
-        case 'aws':
-          core.info('Building with AWS');
-          this.RemoteBuilderProviderPlatform = new AWSBuildPlatform(this.buildParams);
-          break;
-        default:
-        case 'k8s':
-          core.info('Building with Kubernetes');
-          this.RemoteBuilderProviderPlatform = new Kubernetes(this.buildParams);
-          break;
-      }
+      RemoteBuilder.setupBuildPlatform();
 
       await this.RemoteBuilderProviderPlatform.setupSharedBuildResources(
         this.buildId,
         this.buildParams,
-        branchName,
+        this.branchName,
         this.defaultSecrets,
       );
 
@@ -68,40 +56,23 @@ class RemoteBuilder {
       this.projectPathFull = `${this.repoPathFull}/${this.buildParams.projectPath}`;
       this.libraryFolderFull = `${this.projectPathFull}/Library`;
 
-      await RemoteBuilder.SetupStep(branchName);
+      await RemoteBuilder.SetupStep(this.branchName);
       await RemoteBuilder.BuildStep(baseImage);
       await RemoteBuilder.CompressionStep();
-      await RemoteBuilder.UploadArtifacts(branchName);
+      await RemoteBuilder.UploadArtifacts(this.branchName);
       if (this.SteamDeploy) {
         await RemoteBuilder.DeployToSteam();
       }
       await this.RemoteBuilderProviderPlatform.cleanupSharedBuildResources(
         this.buildId,
         this.buildParams,
-        branchName,
+        this.branchName,
         this.defaultSecrets,
       );
     } catch (error) {
-      core.error(JSON.stringify(error, undefined, 4));
-      core.setFailed('Remote Builder failed');
-      await this.RemoteBuilderProviderPlatform.cleanupSharedBuildResources(
-        this.buildId,
-        this.buildParams,
-        branchName,
-        this.defaultSecrets,
-      );
+      await RemoteBuilder.handleException(error);
       throw error;
     }
-  }
-
-  private static newMethod(token: string) {
-    this.defaultSecrets = [
-      {
-        ParameterKey: 'GithubToken',
-        EnvironmentVariable: 'GITHUB_TOKEN',
-        ParameterValue: token,
-      },
-    ];
   }
 
   private static async SetupStep(branchName: string | undefined) {
@@ -164,69 +135,14 @@ class RemoteBuilder {
       ],
       `/${buildVolumeFolder}`,
       `/${buildVolumeFolder}/`,
-      [
-        {
-          name: 'GITHUB_SHA',
-          value: process.env.GITHUB_SHA || '',
-        },
-      ],
+      RemoteBuilder.defaultGitShaEnvironmentVariable,
       this.defaultSecrets,
     );
   }
 
   private static async BuildStep(baseImage: any) {
     const buildSecrets = new Array();
-
-    buildSecrets.push(...this.defaultSecrets);
-
-    if (process.env.UNITY_LICENSE)
-      buildSecrets.push({
-        ParameterKey: 'UnityLicense',
-        EnvironmentVariable: 'UNITY_LICENSE',
-        ParameterValue: process.env.UNITY_LICENSE,
-      });
-
-    if (process.env.UNITY_EMAIL)
-      buildSecrets.push({
-        ParameterKey: 'UnityEmail',
-        EnvironmentVariable: 'UNITY_EMAIL',
-        ParameterValue: process.env.UNITY_EMAIL,
-      });
-
-    if (process.env.UNITY_PASSWORD)
-      buildSecrets.push({
-        ParameterKey: 'UnityPassword',
-        EnvironmentVariable: 'UNITY_PASSWORD',
-        ParameterValue: process.env.UNITY_PASSWORD,
-      });
-
-    if (process.env.UNITY_SERIAL)
-      buildSecrets.push({
-        ParameterKey: 'UnitySerial',
-        EnvironmentVariable: 'UNITY_SERIAL',
-        ParameterValue: process.env.UNITY_SERIAL,
-      });
-
-    if (this.buildParams.androidKeystoreBase64)
-      buildSecrets.push({
-        ParameterKey: 'AndroidKeystoreBase64',
-        EnvironmentVariable: 'ANDROID_KEYSTORE_BASE64',
-        ParameterValue: this.buildParams.androidKeystoreBase64,
-      });
-
-    if (this.buildParams.androidKeystorePass)
-      buildSecrets.push({
-        ParameterKey: 'AndroidKeystorePass',
-        EnvironmentVariable: 'ANDROID_KEYSTORE_PASS',
-        ParameterValue: this.buildParams.androidKeystorePass,
-      });
-
-    if (this.buildParams.androidKeyaliasPass)
-      buildSecrets.push({
-        ParameterKey: 'AndroidKeyAliasPass',
-        EnvironmentVariable: 'AWS_ACCESS_KEY_ALIAS_PASS',
-        ParameterValue: this.buildParams.androidKeyaliasPass,
-      });
+    RemoteBuilder.setupBuildSecrets(buildSecrets);
     core.info('Starting part 2/4 (build unity project)');
     await this.RemoteBuilderProviderPlatform.runBuildTask(
       this.buildId,
@@ -243,60 +159,7 @@ class RemoteBuilder {
       ],
       `/${buildVolumeFolder}`,
       `/${this.repoPathFull}`,
-      [
-        {
-          name: 'ContainerMemory',
-          value: this.buildParams.remoteBuildMemory,
-        },
-        {
-          name: 'ContainerCpu',
-          value: this.buildParams.remoteBuildCpu,
-        },
-        {
-          name: 'GITHUB_WORKSPACE',
-          value: `/${buildVolumeFolder}/${this.buildId}/${repositoryFolder}/`,
-        },
-        {
-          name: 'PROJECT_PATH',
-          value: this.buildParams.projectPath,
-        },
-        {
-          name: 'BUILD_PATH',
-          value: this.buildParams.buildPath,
-        },
-        {
-          name: 'BUILD_FILE',
-          value: this.buildParams.buildFile,
-        },
-        {
-          name: 'BUILD_NAME',
-          value: this.buildParams.buildName,
-        },
-        {
-          name: 'BUILD_METHOD',
-          value: this.buildParams.buildMethod,
-        },
-        {
-          name: 'CUSTOM_PARAMETERS',
-          value: this.buildParams.customParameters,
-        },
-        {
-          name: 'BUILD_TARGET',
-          value: this.buildParams.platform,
-        },
-        {
-          name: 'ANDROID_VERSION_CODE',
-          value: this.buildParams.androidVersionCode.toString(),
-        },
-        {
-          name: 'ANDROID_KEYSTORE_NAME',
-          value: this.buildParams.androidKeystoreName,
-        },
-        {
-          name: 'ANDROID_KEYALIAS_NAME',
-          value: this.buildParams.androidKeyaliasName,
-        },
-      ],
+      RemoteBuilder.readBuildEnvironmentVariables(),
       buildSecrets,
     );
   }
@@ -348,29 +211,8 @@ class RemoteBuilder {
       ],
       `/${buildVolumeFolder}`,
       `/${buildVolumeFolder}/`,
-      [
-        {
-          name: 'GITHUB_SHA',
-          value: process.env.GITHUB_SHA || '',
-        },
-        {
-          name: 'AWS_DEFAULT_REGION',
-          value: process.env.AWS_DEFAULT_REGION || '',
-        },
-      ],
-      [
-        {
-          ParameterKey: 'AWSAccessKeyID',
-          EnvironmentVariable: 'AWS_ACCESS_KEY_ID',
-          ParameterValue: process.env.AWS_ACCESS_KEY_ID || '',
-        },
-        {
-          ParameterKey: 'AWSSecretAccessKey',
-          EnvironmentVariable: 'AWS_SECRET_ACCESS_KEY',
-          ParameterValue: process.env.AWS_SECRET_ACCESS_KEY || '',
-        },
-        ...this.defaultSecrets,
-      ],
+      RemoteBuilder.readUploadArtifactEnvironmentVariables(),
+      RemoteBuilder.readUploadArtifactsSecrets(),
     );
   }
 
@@ -399,39 +241,237 @@ class RemoteBuilder {
           value: process.env.GITHUB_SHA || '',
         },
       ],
-      [
-        {
-          EnvironmentVariable: 'INPUT_APPID',
-          ParameterKey: 'appId',
-          ParameterValue: process.env.APP_ID || '',
-        },
-        {
-          EnvironmentVariable: 'INPUT_BUILDDESCRIPTION',
-          ParameterKey: 'buildDescription',
-          ParameterValue: process.env.BUILD_DESCRIPTION || '',
-        },
-        {
-          EnvironmentVariable: 'INPUT_ROOTPATH',
-          ParameterKey: 'rootPath',
-          ParameterValue: RemoteBuilder.buildParams.buildPath,
-        },
-        {
-          EnvironmentVariable: 'INPUT_RELEASEBRANCH',
-          ParameterKey: 'releaseBranch',
-          ParameterValue: process.env.RELEASE_BRANCH || '',
-        },
-        {
-          EnvironmentVariable: 'INPUT_LOCALCONTENTSERVER',
-          ParameterKey: 'localContentServer',
-          ParameterValue: process.env.LOCAL_CONTENT_SERVER || '',
-        },
-        {
-          EnvironmentVariable: 'INPUT_PREVIEWENABLED',
-          ParameterKey: 'previewEnabled',
-          ParameterValue: process.env.PREVIEW_ENABLED || '',
-        },
-        ...this.defaultSecrets,
-      ],
+      RemoteBuilder.readDeployToSteamSecrets(),
+    );
+  }
+
+  private static setupBuildPlatform() {
+    switch (this.buildParams.remoteBuildCluster) {
+      case 'aws':
+        core.info('Building with AWS');
+        this.RemoteBuilderProviderPlatform = new AWSBuildPlatform(this.buildParams);
+        break;
+      default:
+      case 'k8s':
+        core.info('Building with Kubernetes');
+        this.RemoteBuilderProviderPlatform = new Kubernetes(this.buildParams);
+        break;
+    }
+  }
+
+  private static readRunNumber() {
+    const runNumber = process.env.GITHUB_RUN_NUMBER;
+    if (!runNumber || runNumber === '') {
+      throw new Error('no run number found, exiting');
+    }
+    return runNumber;
+  }
+
+  private static readBranchNameSetting() {
+    const defaultBranchName =
+      process.env.GITHUB_REF?.split('/')
+        .filter((x) => {
+          x = x[0].toUpperCase() + x.slice(1);
+          return x;
+        })
+        .join('') || '';
+    this.branchName =
+      process.env.REMOTE_BUILDER_CACHE !== undefined ? process.env.REMOTE_BUILDER_CACHE : defaultBranchName;
+  }
+
+  private static setupDefaultSecrets() {
+    this.defaultSecrets = [
+      {
+        ParameterKey: 'GithubToken',
+        EnvironmentVariable: 'GITHUB_TOKEN',
+        ParameterValue: this.buildParams.githubToken,
+      },
+    ];
+  }
+
+  private static setupBuildSecrets(buildSecrets: any[]) {
+    buildSecrets.push(...this.defaultSecrets);
+
+    if (process.env.UNITY_LICENSE)
+      buildSecrets.push({
+        ParameterKey: 'UnityLicense',
+        EnvironmentVariable: 'UNITY_LICENSE',
+        ParameterValue: process.env.UNITY_LICENSE,
+      });
+
+    if (process.env.UNITY_EMAIL)
+      buildSecrets.push({
+        ParameterKey: 'UnityEmail',
+        EnvironmentVariable: 'UNITY_EMAIL',
+        ParameterValue: process.env.UNITY_EMAIL,
+      });
+
+    if (process.env.UNITY_PASSWORD)
+      buildSecrets.push({
+        ParameterKey: 'UnityPassword',
+        EnvironmentVariable: 'UNITY_PASSWORD',
+        ParameterValue: process.env.UNITY_PASSWORD,
+      });
+
+    if (process.env.UNITY_SERIAL)
+      buildSecrets.push({
+        ParameterKey: 'UnitySerial',
+        EnvironmentVariable: 'UNITY_SERIAL',
+        ParameterValue: process.env.UNITY_SERIAL,
+      });
+
+    if (this.buildParams.androidKeystoreBase64)
+      buildSecrets.push({
+        ParameterKey: 'AndroidKeystoreBase64',
+        EnvironmentVariable: 'ANDROID_KEYSTORE_BASE64',
+        ParameterValue: this.buildParams.androidKeystoreBase64,
+      });
+
+    if (this.buildParams.androidKeystorePass)
+      buildSecrets.push({
+        ParameterKey: 'AndroidKeystorePass',
+        EnvironmentVariable: 'ANDROID_KEYSTORE_PASS',
+        ParameterValue: this.buildParams.androidKeystorePass,
+      });
+
+    if (this.buildParams.androidKeyaliasPass)
+      buildSecrets.push({
+        ParameterKey: 'AndroidKeyAliasPass',
+        EnvironmentVariable: 'AWS_ACCESS_KEY_ALIAS_PASS',
+        ParameterValue: this.buildParams.androidKeyaliasPass,
+      });
+  }
+
+  private static readUploadArtifactsSecrets() {
+    return [
+      {
+        ParameterKey: 'AWSAccessKeyID',
+        EnvironmentVariable: 'AWS_ACCESS_KEY_ID',
+        ParameterValue: process.env.AWS_ACCESS_KEY_ID || '',
+      },
+      {
+        ParameterKey: 'AWSSecretAccessKey',
+        EnvironmentVariable: 'AWS_SECRET_ACCESS_KEY',
+        ParameterValue: process.env.AWS_SECRET_ACCESS_KEY || '',
+      },
+      ...this.defaultSecrets,
+    ];
+  }
+
+  private static readDeployToSteamSecrets(): RemoteBuilderSecret[] {
+    return [
+      {
+        EnvironmentVariable: 'INPUT_APPID',
+        ParameterKey: 'appId',
+        ParameterValue: process.env.APP_ID || '',
+      },
+      {
+        EnvironmentVariable: 'INPUT_BUILDDESCRIPTION',
+        ParameterKey: 'buildDescription',
+        ParameterValue: process.env.BUILD_DESCRIPTION || '',
+      },
+      {
+        EnvironmentVariable: 'INPUT_ROOTPATH',
+        ParameterKey: 'rootPath',
+        ParameterValue: RemoteBuilder.buildParams.buildPath,
+      },
+      {
+        EnvironmentVariable: 'INPUT_RELEASEBRANCH',
+        ParameterKey: 'releaseBranch',
+        ParameterValue: process.env.RELEASE_BRANCH || '',
+      },
+      {
+        EnvironmentVariable: 'INPUT_LOCALCONTENTSERVER',
+        ParameterKey: 'localContentServer',
+        ParameterValue: process.env.LOCAL_CONTENT_SERVER || '',
+      },
+      {
+        EnvironmentVariable: 'INPUT_PREVIEWENABLED',
+        ParameterKey: 'previewEnabled',
+        ParameterValue: process.env.PREVIEW_ENABLED || '',
+      },
+      ...this.defaultSecrets,
+    ];
+  }
+
+  private static readUploadArtifactEnvironmentVariables() {
+    return [
+      {
+        name: 'GITHUB_SHA',
+        value: process.env.GITHUB_SHA || '',
+      },
+      {
+        name: 'AWS_DEFAULT_REGION',
+        value: process.env.AWS_DEFAULT_REGION || '',
+      },
+    ];
+  }
+
+  private static readBuildEnvironmentVariables(): RemoteBuilderEnvironmentVariable[] {
+    return [
+      {
+        name: 'ContainerMemory',
+        value: this.buildParams.remoteBuildMemory,
+      },
+      {
+        name: 'ContainerCpu',
+        value: this.buildParams.remoteBuildCpu,
+      },
+      {
+        name: 'GITHUB_WORKSPACE',
+        value: `/${buildVolumeFolder}/${this.buildId}/${repositoryFolder}/`,
+      },
+      {
+        name: 'PROJECT_PATH',
+        value: this.buildParams.projectPath,
+      },
+      {
+        name: 'BUILD_PATH',
+        value: this.buildParams.buildPath,
+      },
+      {
+        name: 'BUILD_FILE',
+        value: this.buildParams.buildFile,
+      },
+      {
+        name: 'BUILD_NAME',
+        value: this.buildParams.buildName,
+      },
+      {
+        name: 'BUILD_METHOD',
+        value: this.buildParams.buildMethod,
+      },
+      {
+        name: 'CUSTOM_PARAMETERS',
+        value: this.buildParams.customParameters,
+      },
+      {
+        name: 'BUILD_TARGET',
+        value: this.buildParams.platform,
+      },
+      {
+        name: 'ANDROID_VERSION_CODE',
+        value: this.buildParams.androidVersionCode.toString(),
+      },
+      {
+        name: 'ANDROID_KEYSTORE_NAME',
+        value: this.buildParams.androidKeystoreName,
+      },
+      {
+        name: 'ANDROID_KEYALIAS_NAME',
+        value: this.buildParams.androidKeyaliasName,
+      },
+    ];
+  }
+
+  private static async handleException(error: unknown) {
+    core.error(JSON.stringify(error, undefined, 4));
+    core.setFailed('Remote Builder failed');
+    await this.RemoteBuilderProviderPlatform.cleanupSharedBuildResources(
+      this.buildId,
+      this.buildParams,
+      this.branchName,
+      this.defaultSecrets,
     );
   }
 }
