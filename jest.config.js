22c1b903a46871657936c9293f78beea60392136
Frostebite <jas.f.ukcmti@gmail.com>
2021-09-29 23:51:53 +0100

    Integration test removed, workflows serve as good integration tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
-  modulePathIgnorePatterns: ['src/model/cloud-runner'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
--- src/model/cloud-runner/cloud-runner.int.test.ts
+++
@@ -1,7 +0,0 @@
-describe('Cloud Runner', () => {
-  describe('Check 1', () => {
-    it('succeeds', () => {
-      expect(1).toBe(1);
-    });
-  });
-});
