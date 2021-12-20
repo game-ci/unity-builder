c83cceffae9547c452c926dab69f22bcf75cc07e
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-20 00:04:46 +0000

    Basic cloud runner test

--- src/model/cloud-runner/cloud-runner.test.ts
+++ src/model/cloud-runner/cloud-runner.test.ts
@@ -2,33 +2,30 @@ import { BuildParameters, ImageTag } from '..';
 import CloudRunner from './cloud-runner';
 import Input from '../input';
 
-describe('Cloud Runner', () => {
-  it('builds', async () => {
-    Input.cliOptions = {
-      versioning: 'None',
-      projectPath: 'test-project',
-      customBuildSteps: `
-      - name: 'step 1'
-        image: 'alpine'
-        commands: ['printenv']
-        secrets:
-          - name: 'testCustomSecret'
-            value: 'VALUEXXX'
-      `,
-    };
-    Input.githubEnabled = false;
-    const buildParameter = await BuildParameters.create();
-    const baseImage = new ImageTag(buildParameter);
-
-    await CloudRunner.run(buildParameter, baseImage.toString());
-  }, 500000);
-});
-if (process.env.GITHUB_SHA !== undefined) {
-  describe('Cloud Runner Remote', () => {
+if (process.env.INCLUDE_CLOUD_RUNNER_TEST !== undefined) {
+  describe('Cloud Runner', () => {
     it('builds', async () => {
-      const buildParameter = await BuildParameters.create();
-      const baseImage = new ImageTag(buildParameter);
-      await CloudRunner.run(buildParameter, baseImage.toString());
-    });
+      await runTestBuild();
+    }, 500000);
   });
+}
+
+async function runTestBuild() {
+  Input.cliOptions = {
+    versioning: 'None',
+    projectPath: 'test-project',
+    customBuildSteps: `
+    - name: 'step 1'
+      image: 'alpine'
+      commands: ['printenv']
+      secrets:
+        - name: 'testCustomSecret'
+          value: 'VALUEXXX'
+    `,
+  };
+  Input.githubEnabled = false;
+  const buildParameter = await BuildParameters.create();
+  const baseImage = new ImageTag(buildParameter);
+
+  await CloudRunner.run(buildParameter, baseImage.toString());
 }
