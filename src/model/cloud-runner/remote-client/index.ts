cc8895d935acbeecc4b6021e7b6de035b3672491
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-20 22:55:09 +0000

    Serialize all input and buildparams

--- src/model/cloud-runner/remote-client/index.ts
+++ src/model/cloud-runner/remote-client/index.ts
@@ -1,11 +1,7 @@
-import { CloudRunnerState } from '../state/cloud-runner-state';
 import { DownloadRepository } from './remote-steps/download-repository';
 
 export class RemoteClientCli {
   static async RunRemoteClient(options) {
-    const buff = Buffer.from(process.env.SERIALIZED_BUILD_PARAMS || '', 'base64');
-    const text = buff.toString('ascii');
-    CloudRunnerState.setup(JSON.parse(text));
     switch (options.remoteClientState) {
       default:
         await DownloadRepository.run();
