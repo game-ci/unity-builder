443a0403d5914fb838f775d315b2e1d7c921242e
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-21 23:15:00 +0000

    Setup cloud runner state properly within RemoteClientCli

--- src/model/cloud-runner/remote-client/index.ts
+++ src/model/cloud-runner/remote-client/index.ts
@@ -1,7 +1,11 @@
+import { BuildParameters } from '../..';
+import { CloudRunnerState } from '../state/cloud-runner-state';
 import { DownloadRepository } from './remote-steps/download-repository';
 
 export class RemoteClientCli {
   static async RunRemoteClient(options) {
+    const buildParameter = await BuildParameters.create();
+    CloudRunnerState.setup(buildParameter);
     switch (options.remoteClientState) {
       default:
         await DownloadRepository.run();
