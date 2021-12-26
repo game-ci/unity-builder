ca23c2c303c0c1776b4436fb97868e9fdf19bbc8
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-26 01:48:36 +0000

    test

--- src/model/cli/remote-client/remote-client-system.ts
+++ src/model/cli/remote-client/remote-client-system.ts
@@ -5,17 +5,20 @@ export class RemoteClientSystem {
   public static async Run(command: string) {
     return await new Promise<string>((promise) => {
       let output = '';
-      const child = exec(command);
-      child.stdout?.on('data', function (data) {
-        const outputChunk = `${data}`;
+      const child = exec(command, (error, stdout, stderr) => {
+        if (error) {
+          CloudRunnerLogger.logRemoteCli(`[ERROR] ${error.message}`);
+          throw new Error(error.toString());
+        }
+        if (stderr) {
+          CloudRunnerLogger.logRemoteCli(`[STD-E/DIAG] ${stderr.toString()}`);
+        }
+        const outputChunk = `${stdout.toString()}`;
         CloudRunnerLogger.logRemoteCli(outputChunk);
         output += outputChunk;
       });
-      child.stderr?.on('data', function (data) {
-        CloudRunnerLogger.logRemoteCli(`[STD-ERROR] ${data}`);
-      });
       child.on('close', function (code) {
-        CloudRunnerLogger.logRemoteCli(`${code} `);
+        CloudRunnerLogger.logRemoteCli(`${code}`);
         promise(output);
       });
     });
