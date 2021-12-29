18828c059eeb02373420fef91faf14bddd03a050
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-29 22:11:36 +0000

    Kubernetes use env var based secrets

--- src/model/input-readers/github-cli.ts
+++ src/model/input-readers/github-cli.ts
@@ -1,10 +1,8 @@
-import assert from 'assert';
 import System from '../system';
 
 export class GithubCliReader {
   static async GetGitHubAuthToken() {
     try {
-      assert(await System.run(`gh --help`, [], {}, false));
       return await System.run(`gh auth status -t`, [], {}, false);
     } catch {
       return '';
