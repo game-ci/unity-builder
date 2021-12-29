8c09e24d75ea6010fca0f5d523a2111e12ef6002
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-29 17:50:00 +0000

    Adding remote reader and tests

--- src/model/input-readers/git-repo.ts
+++ src/model/input-readers/git-repo.ts
@@ -1,11 +1,16 @@
+import { assert } from 'console';
+import System from '../system';
+import fs from 'fs';
+
 export class GitRepoReader {
   static GetSha() {
     return '';
   }
-  static GetRemote() {
-    return '';
+  public static async GetRemote() {
+    return (await System.run(`git remote -v`)).split(' ')[1].split('https://github.com/')[1].split('.git')[0];
   }
-  static GetBranch() {
-    return '';
+  public static async GetBranch() {
+    assert(fs.existsSync(`.git`));
+    return (await System.run(`git branch`)).split('*')[1].split(`\n`)[0].replace(/ /g, ``);
   }
 }
