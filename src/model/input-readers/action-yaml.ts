6f7c978890070ffd760f3cc4b8a3412038d82afd
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-29 19:22:17 +0000

    All multi-line parameters in ImageEnvFactory should use --env {paramter} notation rather than --env{name}={value}

--- src/model/input-readers/action-yaml.ts
+++ src/model/input-readers/action-yaml.ts
@@ -1,10 +1,15 @@
 import fs from 'fs';
+import path from 'path';
 import YAML from 'yaml';
 
 export class ActionYamlReader {
   private actionYamlParsed: any;
   public constructor() {
-    this.actionYamlParsed = YAML.parse(fs.readFileSync(`action.yml`).toString());
+    let filename = `action.yml`;
+    if (!fs.existsSync(filename)) {
+      filename = path.join(`..`, filename);
+    }
+    this.actionYamlParsed = YAML.parse(fs.readFileSync(filename).toString());
   }
   public GetActionYamlValue(key: string) {
     return this.actionYamlParsed.inputs[key]?.description || 'No description found in action.yml';
