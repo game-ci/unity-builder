f7757c35c1f70607ef009c48c73b4982793382ec
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-29 20:29:31 +0000

    cli default parameter

--- src/model/cloud-runner/services/cloud-runner-build-command-process.ts
+++ src/model/cloud-runner/services/cloud-runner-build-command-process.ts
@@ -4,8 +4,14 @@ export class CloudRunnerBuildCommandProcessor {
   public static ProcessCommands(commands: string, buildParameters: BuildParameters): string {
     return `echo "---"
       echo "start"
+      ${CloudRunnerBuildCommandProcessor.GetSecrets(buildParameters)}
       ${commands}
       echo "end--${buildParameters.logId}"
     `;
+  }
+  static GetSecrets(buildParameters: BuildParameters) {
+    return buildParameters.cloudRunnerCluster === `k8s`
+      ? `for f in /credentials; do cat $f | base64 && echo $f; done`
+      : ``;
   }
 }
