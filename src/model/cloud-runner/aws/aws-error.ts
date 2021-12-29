d608318778a33f0d65a999064a55d5d4aa02aa47
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-29 14:38:46 +0000

    Cleanup old commmented out code

--- src/model/cloud-runner/aws/aws-error.ts
+++ src/model/cloud-runner/aws/aws-error.ts
@@ -3,24 +3,7 @@ import * as SDK from 'aws-sdk';
 import * as core from '@actions/core';
 
 export class AWSError {
-  static async handleStackCreationFailure(
-    error: any,
-    CF: SDK.CloudFormation,
-    taskDefStackName: string,
-    //taskDefCloudFormation: string,
-    //parameters: any[],
-    //secrets: CloudRunnerSecret[],
-  ) {
-    CloudRunnerLogger.log(' \n ');
-    //CloudRunnerLogger.log('aws stack parameters: ');
-    //CloudRunnerLogger.log(JSON.stringify(parameters, undefined, 4));
-
-    //CloudRunnerLogger.log('aws stack secrets: ');
-    //CloudRunnerLogger.log(JSON.stringify(secrets, undefined, 4));
-
-    //CloudRunnerLogger.log('aws stack: ');
-    //CloudRunnerLogger.log(taskDefCloudFormation);
-
+  static async handleStackCreationFailure(error: any, CF: SDK.CloudFormation, taskDefStackName: string) {
     CloudRunnerLogger.log('aws error: ');
     core.error(JSON.stringify(error, undefined, 4));
     CloudRunnerLogger.log('Getting events and resources for task stack');
