62b4721fa2a99cee2e3a3b1fc1e677b27207941c
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-31 15:17:21 +0000

    cleanup

--- src/model/input-readers/github-cli.ts
+++ src/model/input-readers/github-cli.ts
@@ -3,7 +3,10 @@ import System from '../system';
 export class GithubCliReader {
   static async GetGitHubAuthToken() {
     try {
-      return await System.run(`gh auth status -t`, [], {}, false);
+      return (await System.run(`gh auth status -t`, [], {}, false))
+        .split(`Token: `)[1]
+        .replace(/ /g, '')
+        .replace(/\n/g, '');
     } catch {
       return '';
     }
