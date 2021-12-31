e8e3273cfaaf1384f72557abdb18322ca7a47a68
Frostebite <jas.f.ukcmti@gmail.com>
2021-12-31 19:01:35 +0000

    fix

--- src/model/cloud-runner/services/cloud-runner-build-command-process.ts
+++ src/model/cloud-runner/services/cloud-runner-build-command-process.ts
@@ -3,10 +3,12 @@ import { BuildParameters, Input } from '../..';
 export class CloudRunnerBuildCommandProcessor {
   public static ProcessCommands(commands: string, buildParameters: BuildParameters): string {
     return `echo "---"
+      echo "start cloud runner init"
       ${Input.cloudRunnerTests ? '' : '#'} printenv
-      echo "start"
+      echo "start cloud runner job"
       ${commands}
-      echo "end---${buildParameters.logId}"
+      echo "end of cloud runner job
+      ---${buildParameters.logId}"
     `;
   }
 }
