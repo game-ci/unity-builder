c9dc026ec8cddee7a4dffc4f401a4d35eb617e8c
AndrewKahr <22359829+AndrewKahr@users.noreply.github.com>
2022-01-21 13:21:22 -0800

    Update dist/platforms/windows/steps/return_license.ps1
    Co-authored-by: Webber Takken <webber.nl@gmail.com>

--- dist/platforms/windows/steps/return_license.ps1
+++ dist/platforms/windows/steps/return_license.ps1
@@ -1,4 +1,4 @@
-# Returns the active Unity license
+# Return the active Unity license
 & "C:\Program Files\Unity\Hub\Editor\$Env:UNITY_VERSION\Editor\Unity.exe" -batchmode -quit -nographics `
                                                                           -username $Env:UNITY_USER `
                                                                           -password $Env:UNITY_PASS `
