7433e0f63cd7e222190da8fb11fdf60bc4873945
Frostebite <jas.f.ukcmti@gmail.com>
2022-01-25 01:07:12 +0000

    do not fail from exception in github cli

--- src/model/input-readers/github-cli.ts
+++ src/model/input-readers/github-cli.ts
@@ -1,7 +1,15 @@
 import { CloudRunnerSystem } from '../cli/remote-client/remote-client-services/cloud-runner-system';
+import * as core from '@actions/core';
 
 export class GithubCliReader {
   static async GetGitHubAuthToken() {
-    return (await CloudRunnerSystem.Run(`gh auth status -t`)).split(`Token: `)[1].replace(/ /g, '').replace(/\n/g, '');
+    try {
+      return (await CloudRunnerSystem.Run(`gh auth status -t`))
+        .split(`Token: `)[1]
+        .replace(/ /g, '')
+        .replace(/\n/g, '');
+    } catch (error: any) {
+      core.info(error);
+    }
   }
 }
