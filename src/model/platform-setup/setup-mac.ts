b3c4dc6c2cc455c44fc108faf4467aef5476d376
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2022-01-26 14:14:39 -0800

    Try ts setup again

--- src/model/platform-setup/setup-mac.ts
+++ src/model/platform-setup/setup-mac.ts
@@ -1,8 +1,16 @@
 import { BuildParameters } from '..';
 import { getUnityChangeset } from 'unity-changeset';
+import { exec } from '@actions/exec';
+import fs from 'fs';
 
 class SetupMac {
+  static unityHubPath = '/Applications/Unity\\ Hub.app/Contents/MacOS/Unity\\ Hub';
   public static async setup(buildParameters: BuildParameters, actionFolder: string) {
+    const unityEditorPath = `/Applications/Unity/Hub/Editor/${buildParameters.version}/Unity.app/Contents/MacOS/Unity`;
+    if (!fs.existsSync(unityEditorPath)) {
+      await SetupMac.installUnityHub();
+      await SetupMac.installUnity(buildParameters);
+    }
     const unityChangeset = await getUnityChangeset(buildParameters.version);
 
     //Need to set environment variables from here because we execute
@@ -28,6 +36,23 @@ class SetupMac {
     process.env.ANDROID_SDK_MANAGER_PARAMETERS = buildParameters.androidSdkManagerParameters;
     process.env.CUSTOM_PARAMETERS = buildParameters.customParameters;
     process.env.CHOWN_FILES_TO = buildParameters.chownFilesTo;
+  }
+
+  private static async installUnityHub(silent = false) {
+    const command = 'brew install unity-hub';
+    if (!fs.existsSync(this.unityHubPath)) {
+      await exec(command, [], { silent });
+    }
+  }
+
+  private static async installUnity(buildParameters: BuildParameters, silent = false) {
+    const changeset = await getUnityChangeset(buildParameters.version).changeset;
+    const command = `${this.unityHubPath} -- --headless install \
+                                          --version ${buildParameters.version} \
+                                          --changeset ${changeset} \
+                                          --module mac-il2cpp \
+                                          --childModules`;
+    await exec(command, [], { silent });
   }
 }
 
