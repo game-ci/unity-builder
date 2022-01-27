4a8ee6f820c37d56b8619f85c46f369113cc4bc0
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2022-01-26 22:20:43 -0800

    Fix project path windows

--- dist/platforms/windows/steps/activate.ps1
+++ dist/platforms/windows/steps/activate.ps1
@@ -1,10 +1,7 @@
-Push-Location $ACTIVATE_LICENSE_PATH
-
 # Activates Unity
 & "C:\Program Files\Unity\Hub\Editor\$Env:UNITY_VERSION\Editor\Unity.exe" -batchmode -quit -nographics `
                                                                           -username $Env:UNITY_EMAIL `
                                                                           -password $Env:UNITY_PASSWORD `
                                                                           -serial $Env:UNITY_SERIAL `
+                                                                          -projectPath "c:/BlankProject" `
                                                                           -logfile | Out-Host
-
-Pop-Location
