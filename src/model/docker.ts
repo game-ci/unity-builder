baa853a94d3326d8166d95c68a11a250bba978a1
Paul Pacheco <paulpach@gmail.com>
2022-03-11 10:21:13 -0600

    fix linting errors

--- src/model/docker.ts
+++ src/model/docker.ts
@@ -2,15 +2,15 @@ import { exec } from '@actions/exec';
 import ImageTag from './image-tag';
 import ImageEnvironmentFactory from './image-environment-factory';
 import { existsSync, mkdirSync } from 'fs';
-import { join } from 'path';
+import path from 'path';
 
 class Docker {
   static async build(buildParameters, silent = false) {
-    const { path, dockerfile, baseImage } = buildParameters;
+    const { path: buildPath, dockerfile, baseImage } = buildParameters;
     const { version, platform } = baseImage;
 
     const tag = new ImageTag({ repository: '', name: 'unity-builder', version, platform });
-    const command = `docker build ${path} \
+    const command = `docker build ${buildPath} \
       --file ${dockerfile} \
       --build-arg IMAGE=${baseImage} \
       --tag ${tag}`;
@@ -43,21 +43,22 @@ class Docker {
 
   static getBaseOsSpecificArguments(baseOs, workspace, unitySerial, runnerTemporaryPath, sshAgent): string {
     switch (baseOs) {
-      case 'linux':
-        const github_home = join(runnerTemporaryPath, '_github_home');
-        if (!existsSync(github_home)) mkdirSync(github_home);
-        const github_workflow = join(runnerTemporaryPath, '_github_workflow');
-        if (!existsSync(github_workflow)) mkdirSync(github_workflow);
+      case 'linux': {
+        const githubHome = path.join(runnerTemporaryPath, '_github_home');
+        if (!existsSync(githubHome)) mkdirSync(githubHome);
+        const githubWorkflow = path.join(runnerTemporaryPath, '_github_workflow');
+        if (!existsSync(githubWorkflow)) mkdirSync(githubWorkflow);
 
         return `--env UNITY_SERIAL \
                 --env GITHUB_WORKSPACE=/github/workspace \
                 ${sshAgent ? '--env SSH_AUTH_SOCK=/ssh-agent' : ''} \
                 --volume "/var/run/docker.sock":"/var/run/docker.sock:z" \
-                --volume "${github_home}":"/root:z" \
-                --volume "${github_workflow}":"/github/workflow:z" \
+                --volume "${githubHome}":"/root:z" \
+                --volume "${githubWorkflow}":"/github/workflow:z" \
                 --volume "${workspace}":"/github/workspace:z" \
                 ${sshAgent ? `--volume ${sshAgent}:/ssh-agent` : ''} \
                 ${sshAgent ? '--volume /home/runner/.ssh/known_hosts:/root/.ssh/known_hosts:ro' : ''}`;
+      }
       case 'win32':
         return `--env UNITY_SERIAL="${unitySerial}" \
                 --env GITHUB_WORKSPACE=c:/github/workspace \
