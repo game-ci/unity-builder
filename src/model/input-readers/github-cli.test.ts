fdef5575f83ffb2fa92d18138c495e3105090c33
Webber <webber@takken.io>
2022-03-27 22:00:58 +0200

    feat: jest fails if something gets logged to console

--- src/model/input-readers/github-cli.test.ts
+++ src/model/input-readers/github-cli.test.ts
@@ -2,8 +2,11 @@ import { GithubCliReader } from './github-cli';
 import * as core from '@actions/core';
 
 describe(`github cli`, () => {
-  it(`returns`, async () => {
+  // Todo - We can not assume that everyone has the GitHub cli installed locally.
+  it.skip(`returns`, async () => {
     const token = await GithubCliReader.GetGitHubAuthToken();
+
+    // Todo - use expect(result).toStrictEqual(something)
     core.info(token);
   });
 });
