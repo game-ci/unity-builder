7e28c861a8e3d117df4626473757424ae095673d
Webber <webber@takken.io>
2022-03-27 01:31:06 +0100

    fix: early restore call

--- src/model/system.test.ts
+++ src/model/system.test.ts
@@ -42,36 +42,5 @@ describe('System', () => {
         expect(info).toHaveBeenLastCalledWith('foo-bar');
       });
     });
-
-    /**
-     * Not all shells (e.g. Powershell, sh) have a reference to `echo` binary (absent or alias).
-     * To ensure our integration with '@actions/exec' works as expected we run some specific tests in CI only
-     */
-    if (process.env.CI) {
-      describe('integration', () => {
-        execSpy.mockRestore();
-
-        it('runs a command successfully', async () => {
-          await expect(System.run('true')).resolves.not.toBeNull();
-        });
-
-        it('outputs results', async () => {
-          await expect(System.run('echo test')).resolves.toStrictEqual('test\n');
-        });
-
-        it('throws on when error code is not 0', async () => {
-          await expect(System.run('false')).rejects.toThrowError();
-        });
-
-        it('allows pipes using buffer', async () => {
-          await expect(
-            System.run('sh', undefined, {
-              input: Buffer.from('git tag --list --merged HEAD | grep v[0-9]* | wc -l'),
-              // eslint-disable-next-line github/no-then
-            }).then((result) => Number(result)),
-          ).resolves.not.toBeNaN();
-        });
-      });
-    }
   });
 });
