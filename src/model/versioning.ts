9f3c744503ff6d15719855a1040bae37d9fde894
Kacy-Luker <KacyLuker@Gmail.com>
2022-03-31 10:59:51 -0500

    Fix Version Num to use tags in form of #.#.# or v#.#.#

--- src/model/versioning.ts
+++ src/model/versioning.ts
@@ -273,7 +273,7 @@ export default class Versioning {
    */
   static async hasAnyVersionTags() {
     const numberOfCommitsAsString = await System.run('sh', undefined, {
-      input: Buffer.from('git tag --list --merged HEAD | grep v[0-9]* | wc -l'),
+      input: Buffer.from('git tag --list --merged HEAD | grep -E "v?[0-9]*" | wc -l'),
       cwd: this.projectPath,
       silent: false,
     });
