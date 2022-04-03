b6133c86b3962a8a1598394df25d29dc696d63d1
Mark D <jas.f.ukcmti@gmail.com>
2022-04-03 21:05:03 +0100

    correct new line

--- src/model/cloud-runner/services/cloud-runner-build-command-process.ts
+++ src/model/cloud-runner/services/cloud-runner-build-command-process.ts
@@ -17,8 +17,7 @@ export class CloudRunnerBuildCommandProcessor {
       ${commands}
       ${hooks.filter((x) => x.hook.includes(`after`)).map((x) => x.commands) || ' '}
       echo "end of cloud runner job"
-      ---${buildParameters.logId}"
-    `;
+      ---${buildParameters.logId}"`;
   }
 
   public static getHooks(customJobHooks): Hook[] {
