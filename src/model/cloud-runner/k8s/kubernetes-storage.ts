bd291b3824c035f9eeccb69d1c94c1e8c8e4ba8d
Frostebite <jas.f.ukcmti@gmail.com>
2022-04-05 00:06:34 +0100

    default k8s storage class to pd-standard

--- src/model/cloud-runner/k8s/kubernetes-storage.ts
+++ src/model/cloud-runner/k8s/kubernetes-storage.ts
@@ -85,7 +85,7 @@ class KubernetesStorage {
     };
     pvc.spec = {
       accessModes: ['ReadWriteOnce'],
-      storageClassName: buildParameters.kubeStorageClass === '' ? 'standard' : buildParameters.kubeStorageClass,
+      storageClassName: buildParameters.kubeStorageClass === '' ? 'pd-standard' : buildParameters.kubeStorageClass,
       resources: {
         requests: {
           storage: buildParameters.kubeVolumeSize,
