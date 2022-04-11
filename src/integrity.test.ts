b45c762cc5844298a14b09b0f0628fb4158d4d90
Webber <webber@takken.io>
2022-04-11 23:50:18 +0200

    fix: add integrity test

---
+++ src/integrity.test.ts
@@ -0,0 +1,9 @@
+import { stat } from 'fs/promises';
+
+describe('Integrity tests', () => {
+  describe('package-lock.json', () => {
+    it('does not exist', async () => {
+      await expect(stat(`${process.cwd()}/package-lock.json`)).rejects.toThrowError();
+    });
+  });
+});
