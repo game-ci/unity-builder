0e9c5ba70a42b868f03d0e31b4564fe3dc6c2944
Frostebite <jas.f.ukcmti@gmail.com>
2022-04-21 22:58:58 +0100

    prefer fs/promises

--- src/integrity.test.ts
+++ src/integrity.test.ts
@@ -1,9 +1,9 @@
-import fs from 'fs';
+import { stat } from 'fs/promises';
 
 describe('Integrity tests', () => {
   describe('package-lock.json', () => {
     it('does not exist', async () => {
-      await expect(fs.promises.stat(`${process.cwd()}/package-lock.json`)).rejects.toThrowError();
+      await expect(stat(`${process.cwd()}/package-lock.json`)).rejects.toThrowError();
     });
   });
 });
