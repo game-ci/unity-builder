3e830898816c005b21eb0914973b7f94deaa5462
Frostebite <jas.f.ukcmti@gmail.com>
2022-08-11 04:16:33 +0100

    Fix: read git branch from project path git repo

--- src/model/cloud-runner/providers/aws/aws-task-runner.ts
+++ src/model/cloud-runner/providers/aws/aws-task-runner.ts
@@ -58,7 +58,7 @@ class AWSTaskRunner {
     CloudRunnerLogger.log(
       `Cloud runner job status is running ${(await AWSTaskRunner.describeTasks(cluster, taskArn))?.lastStatus}`,
     );
-    if (CloudRunnerOptions.watchCloudRunnerToEnd) {
+    if (!CloudRunnerOptions.watchCloudRunnerToEnd) {
       const shouldCleanup: boolean = false;
       const output: string = '';
 
