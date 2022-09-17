d0b850169b094dd9aff8059a96cc466db67e7211
Frostebite <jas.f.ukcmti@gmail.com>
2022-09-17 02:18:57 +0100

    Remove unintended change

--- dist/default-build-script/Assets/Editor/UnityBuilderAction/Input/AndroidSettings.cs
+++ dist/default-build-script/Assets/Editor/UnityBuilderAction/Input/AndroidSettings.cs
@@ -4,24 +4,21 @@ using UnityEditor;
 
 namespace UnityBuilderAction.Input
 {
-  public class AndroidSettings
-  {
-    public static void Apply(Dictionary<string, string> options)
+    public class AndroidSettings
     {
-      EditorUserBuildSettings.buildAppBundle = options["customBuildPath"].EndsWith(".aab");
-      if (options.TryGetValue("androidKeystoreName", out string keystoreName) && !string.IsNullOrEmpty(keystoreName))
-      {
-        PlayerSettings.Android.useCustomKeystore = true;
-        PlayerSettings.Android.keystoreName = keystoreName;
-      }
-      if (options.TryGetValue("androidKeystorePass", out string keystorePass) && !string.IsNullOrEmpty(keystorePass))
-        PlayerSettings.Android.keystorePass = keystorePass;
-      if (options.TryGetValue("androidKeyaliasName", out string keyaliasName) && !string.IsNullOrEmpty(keyaliasName))
-        PlayerSettings.Android.keyaliasName = keyaliasName;
-      if (options.TryGetValue("androidKeyaliasPass", out string keyaliasPass) && !string.IsNullOrEmpty(keyaliasPass))
-        PlayerSettings.Android.keyaliasPass = keyaliasPass;
-      if (options.TryGetValue("androidTargetSdkVersion", out string androidTargetSdkVersion) && !string.IsNullOrEmpty(androidTargetSdkVersion))
-        PlayerSettings.Android.targetSdkVersion = (AndroidSdkVersions) Enum.Parse(typeof(AndroidSdkVersions), androidTargetSdkVersion);
+        public static void Apply(Dictionary<string, string> options)
+        {
+            EditorUserBuildSettings.buildAppBundle = options["customBuildPath"].EndsWith(".aab");
+            if (options.TryGetValue("androidKeystoreName", out string keystoreName) && !string.IsNullOrEmpty(keystoreName))
+                PlayerSettings.Android.keystoreName = keystoreName;
+            if (options.TryGetValue("androidKeystorePass", out string keystorePass) && !string.IsNullOrEmpty(keystorePass))
+                PlayerSettings.Android.keystorePass = keystorePass;
+            if (options.TryGetValue("androidKeyaliasName", out string keyaliasName) && !string.IsNullOrEmpty(keyaliasName))
+                PlayerSettings.Android.keyaliasName = keyaliasName;
+            if (options.TryGetValue("androidKeyaliasPass", out string keyaliasPass) && !string.IsNullOrEmpty(keyaliasPass))
+                PlayerSettings.Android.keyaliasPass = keyaliasPass;
+            if (options.TryGetValue("androidTargetSdkVersion", out string androidTargetSdkVersion) && !string.IsNullOrEmpty(androidTargetSdkVersion))
+                PlayerSettings.Android.targetSdkVersion = (AndroidSdkVersions)Enum.Parse(typeof(AndroidSdkVersions), androidTargetSdkVersion);
+        }
     }
-  }
 }
