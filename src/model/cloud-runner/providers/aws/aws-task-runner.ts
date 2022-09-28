1b09122e351e38cadc8ab1c25cc06ff580fdd1e6
Frostebite <jas.f.ukcmti@gmail.com>
2022-09-28 03:17:20 +0100

    remove logs

--- src/model/cloud-runner/providers/aws/aws-task-runner.ts
+++ src/model/cloud-runner/providers/aws/aws-task-runner.ts
@@ -30,7 +30,6 @@ class AWSTaskRunner {
     const streamName =
       taskDef.taskDefResources?.find((x) => x.LogicalResourceId === 'KinesisStream')?.PhysicalResourceId || '';
 
-    core.info(`AWS Run Env ${JSON.stringify(environment)}`);
     const task = await AWSTaskRunner.ECS.runTask({
       cluster,
       taskDefinition,
