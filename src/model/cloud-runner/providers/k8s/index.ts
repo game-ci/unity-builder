2bfe623bb7205530ba6aa8abfcb04bd4e134c589
Frostebite <jas.f.ukcmti@gmail.com>
2022-09-30 01:31:01 +0100

    Better locking

--- src/model/cloud-runner/providers/k8s/index.ts
+++ src/model/cloud-runner/providers/k8s/index.ts
@@ -95,6 +95,7 @@ class Kubernetes implements ProviderInterface {
       this.secretName = `build-credentials-${buildGuid}`;
       this.jobName = `unity-builder-job-${buildGuid}`;
       this.containerName = `main`;
+      CloudRunnerLogger.log(`Creating build Secret ${JSON.stringify(this.secretName, undefined, 4)}`);
       await KubernetesSecret.createSecret(secrets, this.secretName, this.namespace, this.kubeClient);
       const jobSpec = KubernetesJobSpecFactory.getJobSpec(
         commands,
@@ -110,10 +111,11 @@ class Kubernetes implements ProviderInterface {
         this.jobName,
         k8s,
       );
+      CloudRunnerLogger.log(`Creating build job ${JSON.stringify(jobSpec, undefined, 4)}`);
 
       // Run
       const jobResult = await this.kubeClientBatch.createNamespacedJob(this.namespace, jobSpec);
-      CloudRunnerLogger.log(`Creating build job ${JSON.stringify(jobResult.body.metadata, undefined, 4)}`);
+      CloudRunnerLogger.log(`Created build job ${JSON.stringify(jobResult.body.metadata, undefined, 4)}`);
 
       await new Promise((promise) => setTimeout(promise, 5000));
       CloudRunnerLogger.log('Job created');
