71f15c363fc95aa969e7082b620c05c87238526a
Frostebite <jas.f.ukcmti@gmail.com>
2022-10-06 22:35:52 +0100

    custom hook files and test

--- src/model/cloud-runner/tests/cloud-runner-run-once-custom-hooks.test.ts
+++ src/model/cloud-runner/tests/cloud-runner-run-once-custom-hooks.test.ts
@@ -19,28 +19,23 @@ describe('Cloud Runner Custom Hooks', () => {
   it('Responds', () => {});
   setups();
   if (CloudRunnerOptions.cloudRunnerTests && CloudRunnerOptions.cloudRunnerCluster !== `k8s`) {
-    it('Run one build it should not already be retained, run subsequent build which should use retained workspace', async () => {
+    it('Check for pre and post custom hooks run contents', async () => {
       const overrides = {
         versioning: 'None',
         projectPath: 'test-project',
         unityVersion: UnityVersioning.determineUnityVersion('test-project', UnityVersioning.read('test-project')),
         targetPlatform: 'StandaloneLinux64',
         cacheKey: `test-case-${uuidv4()}`,
-        retainWorkspaces: true,
       };
       const buildParameter2 = await CreateParameters(overrides);
       const baseImage2 = new ImageTag(buildParameter2);
       const results2 = await CloudRunner.run(buildParameter2, baseImage2.toString());
       CloudRunnerLogger.log(`run 2 succeeded`);
 
-      const build2ContainsRetainedWorkspacePhrase = results2.includes(`Retained Workspace:`);
-      const build2ContainsWorkspaceExistsAlreadyPhrase = results2.includes(`Retained Workspace Already Exists!`);
       const build2ContainsBuildSucceeded = results2.includes('Build succeeded');
       const build2ContainsPreBuildHookMessage = results2.includes('pre-build test!');
       const build2ContainsPostBuildHookMessage = results2.includes('post-build test!');
 
-      expect(build2ContainsRetainedWorkspacePhrase).toBeTruthy();
-      expect(build2ContainsWorkspaceExistsAlreadyPhrase).toBeTruthy();
       expect(build2ContainsBuildSucceeded).toBeTruthy();
       expect(build2ContainsPreBuildHookMessage).toBeTruthy();
       expect(build2ContainsPostBuildHookMessage).toBeTruthy();
