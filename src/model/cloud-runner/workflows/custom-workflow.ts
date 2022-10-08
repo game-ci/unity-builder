5f3c20455099519c978e5c8fe7c073bf4587f609
Frostebite <jas.f.ukcmti@gmail.com>
2022-10-08 06:12:12 +0100

    Custom step file support

--- src/model/cloud-runner/workflows/custom-workflow.ts
+++ src/model/cloud-runner/workflows/custom-workflow.ts
@@ -26,11 +26,10 @@ export class CustomWorkflow {
     try {
       CloudRunnerLogger.log(`Cloud Runner is running in custom job mode`);
       let output = '';
+      if (CloudRunner.buildParameters?.cloudRunnerIntegrationTests) {
+        CloudRunnerLogger.log(`Custom Job Description \n${JSON.stringify(buildSteps, undefined, 4)}`);
+      }
       for (const step of buildSteps) {
-        if (CloudRunner.buildParameters?.cloudRunnerIntegrationTests) {
-          CloudRunnerLogger.log(`Custom Job Description \n ${JSON.stringify(step, undefined, 4)}`);
-        }
-
         output += await CloudRunner.Provider.runTask(
           CloudRunner.buildParameters.buildGuid,
           step.image,
