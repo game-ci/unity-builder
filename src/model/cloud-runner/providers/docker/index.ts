aee2bc65a8f54b3cab48a3f174dc3a83bfd21ffa
Frostebite <jas.f.ukcmti@gmail.com>
2022-10-21 15:22:56 +0100

    fix: garbage collect not implemented error

--- src/model/cloud-runner/providers/docker/index.ts
+++ src/model/cloud-runner/providers/docker/index.ts
@@ -144,17 +144,6 @@ cp -a ${sharedFolder}. /github/workspace/cloud-runner-cache/
       true,
     );
 
-    if (
-      !this.buildParameters?.retainWorkspace &&
-      fs.existsSync(
-        `${workspace}/cloud-runner-cache/${path.basename(CloudRunnerFolders.uniqueCloudRunnerJobFolderAbsolute)}`,
-      )
-    ) {
-      await CloudRunnerSystem.Run(
-        `rm -r ${workspace}/cloud-runner-cache/${path.basename(CloudRunnerFolders.uniqueCloudRunnerJobFolderAbsolute)}`,
-      );
-    }
-
     return myOutput;
   }
 }
