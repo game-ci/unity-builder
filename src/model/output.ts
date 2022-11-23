5bd589e19fd9307a5ac3b294e47ccbba7426e51b
AndrewKahr <22359829+AndrewKahr@users.noreply.github.com>
2022-11-23 02:06:58 -0700

    feat: Android updates for Windows and androidVersionCode output (#478)
    * Create android keystore on windows, output android version code
    
    * Add androidVersionCode output test
    
    * Move android keystore decode logic to TS

--- src/model/output.ts
+++ src/model/output.ts
@@ -4,6 +4,10 @@ class Output {
   static async setBuildVersion(buildVersion) {
     await core.setOutput('buildVersion', buildVersion);
   }
+
+  static async setAndroidVersionCode(androidVersionCode) {
+    await core.setOutput('androidVersionCode', androidVersionCode);
+  }
 }
 
 export default Output;
