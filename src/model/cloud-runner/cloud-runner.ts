c58553c9d83650f989651c93b576728908cd7bc7
Frostebite <jas.f.ukcmti@gmail.com>
2023-02-07 02:21:20 +0000

    async copy unity email,pass,serial

--- src/model/cloud-runner/cloud-runner.ts
+++ src/model/cloud-runner/cloud-runner.ts
@@ -148,7 +148,12 @@ class CloudRunner {
 
       return output;
     } catch (error) {
-      await GitHub.updateGitHubCheck(CloudRunner.buildParameters.buildGuid, error, `failure`, `completed`);
+      await GitHub.updateGitHubCheck(
+        CloudRunner.buildParameters.buildGuid,
+        JSON.stringify(error, undefined, 4),
+        `failure`,
+        `completed`,
+      );
       if (!CloudRunner.buildParameters.isCliMode) core.endGroup();
       await CloudRunnerError.handleException(error, CloudRunner.buildParameters, CloudRunner.defaultSecrets);
       throw error;
