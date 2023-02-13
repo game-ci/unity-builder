5e850a4b9b7375dce4f1102fafa617fd5166e195
Frostebite <jas.f.ukcmti@gmail.com>
2023-02-13 14:54:25 +0000

    extract lib in caching flow

--- src/model/cloud-runner/tests/e2e/cloud-runner-end2end-caching.test.ts
+++ src/model/cloud-runner/tests/e2e/cloud-runner-end2end-caching.test.ts
@@ -52,6 +52,7 @@ describe('Cloud Runner Caching', () => {
       await CloudRunnerSystem.Run(
         `cp ./cloud-runner-cache/cache/${buildParameter.cacheKey}/Library/lib-${buildParameter.buildGuid}.tar ./`,
       );
+      await CloudRunnerSystem.Run(`tree -L 1`);
       await CloudRunnerSystem.Run(`tar -xf lib-${buildParameter.buildGuid}.tar -C ./results`);
       await CloudRunnerSystem.Run(`tree ./results`);
       if (CloudRunnerOptions.cloudRunnerCluster === `local-docker`) {
