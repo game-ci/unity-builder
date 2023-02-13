894cc626c782eaf4dc2c3c971571105841fa376c
Frostebite <jas.f.ukcmti@gmail.com>
2023-02-13 20:52:19 +0000

    locking test improvements

--- src/model/cloud-runner/tests/cloud-runner-locking.test.ts
+++ src/model/cloud-runner/tests/cloud-runner-locking.test.ts
@@ -88,6 +88,89 @@ describe('Cloud Runner Locking', () => {
         await SharedWorkspaceLocking.GetOrCreateLockedWorkspace(newWorkspaceName, runId, buildParameters),
       ).toBeTruthy();
     }, 150000);
+    it(`Get Or Create From No Workspace`, async () => {
+      Cli.options.retainWorkspaces = true;
+      const overrides: any = {
+        versioning: 'None',
+        projectPath: 'test-project',
+        unityVersion: UnityVersioning.determineUnityVersion('test-project', UnityVersioning.read('test-project')),
+        targetPlatform: 'StandaloneLinux64',
+        cacheKey: `test-case-${uuidv4()}`,
+        retainWorkspaces: true,
+      };
+      const buildParameters = await CreateParameters(overrides);
+
+      const newWorkspaceName = `test-workspace-${uuidv4()}`;
+      const runId = uuidv4();
+      CloudRunner.buildParameters = buildParameters;
+      expect(
+        await SharedWorkspaceLocking.GetOrCreateLockedWorkspace(newWorkspaceName, runId, buildParameters),
+      ).toBeTruthy();
+    }, 150000);
+    it(`Get Or Create From Unlocked`, async () => {
+      Cli.options.retainWorkspaces = true;
+      const overrides: any = {
+        versioning: 'None',
+        projectPath: 'test-project',
+        unityVersion: UnityVersioning.determineUnityVersion('test-project', UnityVersioning.read('test-project')),
+        targetPlatform: 'StandaloneLinux64',
+        cacheKey: `test-case-${uuidv4()}`,
+        retainWorkspaces: true,
+      };
+      const buildParameters = await CreateParameters(overrides);
+
+      const newWorkspaceName = `test-workspace-${uuidv4()}`;
+      const runId = uuidv4();
+      CloudRunner.buildParameters = buildParameters;
+      expect(await SharedWorkspaceLocking.CreateWorkspace(newWorkspaceName, buildParameters)).toBeTruthy();
+      expect(
+        await SharedWorkspaceLocking.GetOrCreateLockedWorkspace(newWorkspaceName, runId, buildParameters),
+      ).toBeTruthy();
+    }, 150000);
+    it(`Get Or Create From Locked`, async () => {
+      Cli.options.retainWorkspaces = true;
+      const overrides: any = {
+        versioning: 'None',
+        projectPath: 'test-project',
+        unityVersion: UnityVersioning.determineUnityVersion('test-project', UnityVersioning.read('test-project')),
+        targetPlatform: 'StandaloneLinux64',
+        cacheKey: `test-case-${uuidv4()}`,
+        retainWorkspaces: true,
+      };
+      const buildParameters = await CreateParameters(overrides);
+
+      const newWorkspaceName = `test-workspace-${uuidv4()}`;
+      const runId = uuidv4();
+      CloudRunner.buildParameters = buildParameters;
+      expect(await SharedWorkspaceLocking.CreateWorkspace(newWorkspaceName, buildParameters)).toBeTruthy();
+      expect(await SharedWorkspaceLocking.LockWorkspace(newWorkspaceName, runId, buildParameters)).toBeTruthy();
+      expect(await SharedWorkspaceLocking.DoesWorkspaceExist(newWorkspaceName, buildParameters)).toBeTruthy();
+      expect(
+        await SharedWorkspaceLocking.GetOrCreateLockedWorkspace(newWorkspaceName, runId, buildParameters),
+      ).toBeTruthy();
+    }, 150000);
+    it(`Get Or Create From Unlocked Was Locked`, async () => {
+      Cli.options.retainWorkspaces = true;
+      const overrides: any = {
+        versioning: 'None',
+        projectPath: 'test-project',
+        unityVersion: UnityVersioning.determineUnityVersion('test-project', UnityVersioning.read('test-project')),
+        targetPlatform: 'StandaloneLinux64',
+        cacheKey: `test-case-${uuidv4()}`,
+        retainWorkspaces: true,
+      };
+      const buildParameters = await CreateParameters(overrides);
+
+      const newWorkspaceName = `test-workspace-${uuidv4()}`;
+      const runId = uuidv4();
+      CloudRunner.buildParameters = buildParameters;
+      expect(await SharedWorkspaceLocking.CreateWorkspace(newWorkspaceName, buildParameters)).toBeTruthy();
+      expect(await SharedWorkspaceLocking.LockWorkspace(newWorkspaceName, runId, buildParameters)).toBeTruthy();
+      expect(await SharedWorkspaceLocking.ReleaseWorkspace(newWorkspaceName, runId, buildParameters)).toBeTruthy();
+      expect(
+        await SharedWorkspaceLocking.GetOrCreateLockedWorkspace(newWorkspaceName, runId, buildParameters),
+      ).toBeTruthy();
+    }, 150000);
     it.skip('All Locking Actions', async () => {
       Cli.options.retainWorkspaces = true;
       const overrides: any = {
