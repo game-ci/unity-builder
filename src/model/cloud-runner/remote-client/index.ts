5208eabcbf81b4484d8169ac1a3844bda78b31fe
Frostebite <jas.f.ukcmti@gmail.com>
2023-05-07 16:35:38 +0100

    check for startup message in workflows

--- src/model/cloud-runner/remote-client/index.ts
+++ src/model/cloud-runner/remote-client/index.ts
@@ -16,6 +16,7 @@ import BuildParameters from '../../build-parameters';
 export class RemoteClient {
   @CliFunction(`remote-cli-pre-build`, `sets up a repository, usually before a game-ci build`)
   static async runRemoteClientJob() {
+    await CloudRunnerSystem.Run(`sudo sysctl kernel.dmesg_restrict=0`, false, true);
     assert(
       (await CloudRunnerSystem.Run(`dmesg`, false, true)).includes(`cloud runner build workflow starting`),
       'System logs must include startup message',
