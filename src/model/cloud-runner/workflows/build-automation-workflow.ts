fcf7fde7a0404b853b2d4ec4a4489c8cb1dc3ab1
Frostebite <jas.f.ukcmti@gmail.com>
2023-05-13 22:02:07 +0100

    log file path

--- src/model/cloud-runner/workflows/build-automation-workflow.ts
+++ src/model/cloud-runner/workflows/build-automation-workflow.ts
@@ -92,9 +92,8 @@ export class BuildAutomationWorkflow implements WorkflowInterface {
 
     return `export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 ${cloneBuilderCommands}
-chmod -R a+rX /data/*
-chmod -R 755 /data
-chmod -R 755 /data/*
+chmod -R a+rX ${CloudRunnerFolders.ToLinuxFolder(CloudRunnerFolders.uniqueCloudRunnerJobFolderAbsolute)}
+chmod -R 755 ${CloudRunnerFolders.ToLinuxFolder(CloudRunnerFolders.uniqueCloudRunnerJobFolderAbsolute)}
 node ${builderPath} -m remote-cli-pre-build`;
   }
 
