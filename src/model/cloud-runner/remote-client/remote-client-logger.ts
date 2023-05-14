36dc42ccedc107ed116fc8b75f3f9de138becd0a
Frostebite <jas.f.ukcmti@gmail.com>
2023-05-14 02:37:20 +0100

    log file path

--- src/model/cloud-runner/remote-client/remote-client-logger.ts
+++ src/model/cloud-runner/remote-client/remote-client-logger.ts
@@ -10,9 +10,6 @@ export class RemoteClientLogger {
 
   public static log(message: string) {
     const finalMessage = `[Client] ${message}`;
-    if (!fs.existsSync(this.LogFilePath)) {
-      fs.writeFileSync(this.LogFilePath, ``);
-    }
     this.appendToFile(finalMessage);
     CloudRunnerLogger.log(finalMessage);
   }
