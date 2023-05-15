aec5ebdef99569bfaa400d692e0860585fe8b6d3
Frostebite <jas.f.ukcmti@gmail.com>
2023-05-15 20:14:41 +0100

    log file path

--- src/model/cloud-runner/workflows/build-automation-workflow.ts
+++ src/model/cloud-runner/workflows/build-automation-workflow.ts
@@ -108,8 +108,10 @@ node ${builderPath} -m remote-cli-pre-build`;
     cp -r "${CloudRunnerFolders.ToLinuxFolder(path.join(ubuntuPlatformsFolder, 'steps'))}" "/steps"
     chmod -R +x "/entrypoint.sh"
     chmod -R +x "/steps"
-    echo "game ci start" | tee /home/job-log.txt
-    /entrypoint.sh | tee /home/job-log.txt
+    echo "game ci start"
+    echo "game ci start" >> /home/job-log.txt
+    /entrypoint.sh | tee /home/build-log.txt
+    cat /home/build-log.txt >> /home/job-log.txt
     node ${builderPath} -m remote-cli-post-build`;
   }
 }
