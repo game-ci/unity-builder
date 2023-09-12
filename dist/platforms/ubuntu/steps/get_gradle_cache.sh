13a990f1b5d7f916012cce7db2ae8d23b9315f69
ninetyninereds <idosh@kooply.com>
2023-09-12 23:54:25 +0300

    added clearer printing

--- dist/platforms/ubuntu/steps/get_gradle_cache.sh
+++ dist/platforms/ubuntu/steps/get_gradle_cache.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo "*********************************** Reading the Gradle Cache: ****************************************
 ls -la ~/.gradle
--- dist/platforms/ubuntu/steps/set_gradle_cache.sh
+++ dist/platforms/ubuntu/steps/set_gradle_cache.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo "*********************************** Updating the Gradle Cache: ****************************************
 ls -la ~/.gradle
