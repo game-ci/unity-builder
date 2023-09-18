d65cb6db5738f22aa261ee49ef7649343f4cab62
simensan <simen.is@gmail.com>
2023-09-18 10:36:38 +0200

    Add releasing license when using licensing server

--- dist/platforms/mac/steps/return_license.sh
+++ dist/platforms/mac/steps/return_license.sh
@@ -4,15 +4,25 @@
 echo "Changing to \"$ACTIVATE_LICENSE_PATH\" directory."
 pushd "$ACTIVATE_LICENSE_PATH"
 
-/Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity \
-  -logFile - \
-  -batchmode \
-  -nographics \
-  -quit \
-  -username "$UNITY_EMAIL" \
-  -password "$UNITY_PASSWORD" \
-  -returnlicense \
-  -projectPath "$ACTIVATE_LICENSE_PATH"
 
-# Return to previous working directory
+if [[ -n "$UNITY_LICENSING_SERVER" ]]; then  #
+  #
+  # Return any floating license used.
+  #
+  echo "Returning floating license: \"$FLOATING_LICENSE\""
+  /Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/Frameworks/UnityLicensingClient.app/Contents/MacOS/Unity.Licensing.Client --return-floating "$FLOATING_LICENSE"
+else 
+  /Applications/Unity/Hub/Editor/$UNITY_VERSION/Unity.app/Contents/MacOS/Unity \
+    -logFile - \
+    -batchmode \
+    -nographics \
+    -quit \
+    -username "$UNITY_EMAIL" \
+    -password "$UNITY_PASSWORD" \
+    -returnlicense \
+    -projectPath "$ACTIVATE_LICENSE_PATH"
+
+    # Return to previous working directory
+fi
+
 popd
