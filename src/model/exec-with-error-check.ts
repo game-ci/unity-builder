4dd5a3fb85021a48b2ce3314cfa481ff7d4cacd3
Toby Harris <operator@tobyz.net>
2023-09-21 14:17:50 +0100

    GAME_CI_BUILD_SUCCESS and GAME_CI_STEP_SUMMARY

--- src/model/exec-with-error-check.ts
+++ src/model/exec-with-error-check.ts
@@ -1,4 +1,5 @@
 import { getExecOutput, ExecOptions } from '@actions/exec';
+import { summary } from '@actions/core';
 
 export async function execWithErrorCheck(
   commandLine: string,
@@ -12,18 +13,35 @@ export async function execWithErrorCheck(
     return result.exitCode;
   }
 
-  // Check for errors in the Build Results section
-  const match = result.stdout.match(/^#\s*Build results\s*#(.*)^Size:/ms);
+  // Check for errors in the Unity Build Results section
+  const unityMatch = result.stdout.match(/^#\s*Build results\s*#(.*)^Size:/ms);
 
-  if (match) {
-    const buildResults = match[1];
+  if (unityMatch) {
+    const buildResults = unityMatch[1];
     const errorMatch = buildResults.match(/^Errors:\s*(\d+)$/m);
     if (errorMatch && Number.parseInt(errorMatch[1], 10) !== 0) {
       throw new Error(`There was an error building the project. Please read the logs for details.`);
     }
-  } else {
-    throw new Error(`There was an error building the project. Please read the logs for details.`);
+    await summary.addHeading('Build Results').addQuote(unityMatch[0]).write();
+
+    return result.exitCode;
   }
 
-  return result.exitCode;
+  // Check for presence of game-ci message(s)
+  const gameciMatch = result.stdout.match(/^GAME_CI_BUILD_SUCCESS$/ms);
+
+  if (gameciMatch) {
+    const quote =
+      result.stdout
+        .match(/^GAME_CI_STEP_SUMMARY.*$/gm)
+        ?.map((x) => x.replace('GAME_CI_STEP_SUMMARY', ''))
+        .join('\n\n') ?? '<No GAME_CI_STEP_SUMMARY messages found>';
+    await summary.addHeading('Build Results').addQuote(quote).write();
+
+    return result.exitCode;
+  }
+
+  throw new Error(
+    `There was an error building the project. Did not find success messages in logs (either Unity's build results or GAME_CI_BUILD_SUCCESS).`,
+  );
 }
