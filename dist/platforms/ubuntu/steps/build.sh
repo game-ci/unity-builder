ec3b9b53e39dd68423c722aa0e641834a600c908
Ely Ronnen <elyronnen@gmail.com>
2023-10-04 02:11:46 +0300

    fix android sdkmanager invocation
    Fix "java.lang.NoClassDefFoundError:
    javax/xml/bind/annotation/XmlSchema" error caused by invoking the wrongf
    sdkmanager script

--- dist/platforms/ubuntu/steps/build.sh
+++ dist/platforms/ubuntu/steps/build.sh
@@ -69,7 +69,15 @@ fi
 if [[ "$BUILD_TARGET" == "Android" && -n "$ANDROID_SDK_MANAGER_PARAMETERS" ]]; then
   echo "Updating Android SDK with parameters: $ANDROID_SDK_MANAGER_PARAMETERS"
   export JAVA_HOME="$(awk -F'=' '/JAVA_HOME=/{print $2}' /usr/bin/unity-editor.d/*)"
-  "$(awk -F'=' '/ANDROID_HOME=/{print $2}' /usr/bin/unity-editor.d/*)/tools/bin/sdkmanager" "$ANDROID_SDK_MANAGER_PARAMETERS"
+  ANDROID_HOME_DIRECTORY="$(awk -F'=' '/ANDROID_HOME=/{print $2}' /usr/bin/unity-editor.d/*)"
+  SDKMANAGER=$(find $ANDROID_HOME_DIRECTORY/cmdline-tools -name sdkmanager)
+  if [ -z "${SDKMANAGER}" ]
+  then
+    echo "No sdkmanager found"
+    exit 1
+  fi
+
+  $SDKMANAGER "$ANDROID_SDK_MANAGER_PARAMETERS"
   echo "Updated Android SDK."
 else
   echo "Not updating Android SDK."
