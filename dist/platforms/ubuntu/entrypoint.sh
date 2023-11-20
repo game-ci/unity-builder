36e767467c4ac6cbc3f573e4dfc6cd1c086c66fc
Andrew Kahr <22359829+AndrewKahr@users.noreply.github.com>
2023-11-19 23:08:06 -0800

    Fix variables

--- dist/platforms/ubuntu/entrypoint.sh
+++ dist/platforms/ubuntu/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Get host user/group info so we create files with the correct ownership
-USERNAME=stat -c '%U' "$GITHUB_WORKSPACE/$PROJECT_PATH"
-USERID=stat -c '%u' "$GITHUB_WORKSPACE/$PROJECT_PATH"
-GROUPNAME=stat -c '%G' "$GITHUB_WORKSPACE/$PROJECT_PATH"
-GROUPID=stat -c '%g' "$GITHUB_WORKSPACE/$PROJECT_PATH"
+USERNAME=$(stat -c '%U' "$GITHUB_WORKSPACE/$PROJECT_PATH")
+USERID=$(stat -c '%u' "$GITHUB_WORKSPACE/$PROJECT_PATH")
+GROUPNAME=$(stat -c '%G' "$GITHUB_WORKSPACE/$PROJECT_PATH")
+GROUPID=$(stat -c '%g' "$GITHUB_WORKSPACE/$PROJECT_PATH")
 
 useradd -u $USERID -g $GROUPID $USERNAME
 usermod -aG $GROUPNAME $USERNAME
