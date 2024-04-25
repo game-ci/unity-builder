cda084b313476708c98c80674957abb642419639
fkjzr <filipkajzer@gmail.com>
2024-04-25 20:08:06 +0200

    fix(windows): replacing of urls if gitPrivateToken is set

--- dist/platforms/windows/set_gitcredential.ps1
+++ dist/platforms/windows/set_gitcredential.ps1
@@ -5,12 +5,12 @@ else {
     Write-Host "GIT_PRIVATE_TOKEN is set configuring git credentials"
 
     git config --global credential.helper store
-    git config --global --replace-all "url.https://token:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
-    git config --global --add "url.https://token:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "git@github.com"
-    git config --global --add "url.https://token:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "https://github.com/"
+    git config --global --replace-all url."https://token:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
+    git config --global --add url."https://token:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "git@github.com"
+    git config --global --add url."https://token:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "https://github.com/"
 
-    git config --global "url.https://ssh:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
-    git config --global "url.https://git:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "git@github.com:"
+    git config --global url."https://ssh:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "ssh://git@github.com/"
+    git config --global url."https://git:$env:GIT_PRIVATE_TOKEN@github.com/".insteadOf "git@github.com:"
 }
 
 Write-Host "---------- git config --list -------------"
