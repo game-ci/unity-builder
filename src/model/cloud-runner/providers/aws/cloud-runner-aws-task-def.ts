bb2bf25a9ccd5e167484d2bfac917f7f2650cc66
Matheus Costa <matheus.design12@gmail.com>
2025-04-03 15:33:10 -0300

    fix(aws): keeping the syntax simpler for arrays

--- src/model/cloud-runner/providers/aws/cloud-runner-aws-task-def.ts
+++ src/model/cloud-runner/providers/aws/cloud-runner-aws-task-def.ts
@@ -3,7 +3,7 @@ import { StackResource } from '@aws-sdk/client-cloudformation';
 class CloudRunnerAWSTaskDef {
   public taskDefStackName!: string;
   public taskDefCloudFormation!: string;
-  public taskDefResources: Array<StackResource> | undefined;
-  public baseResources: Array<StackResource> | undefined;
+  public taskDefResources: StackResource[] | undefined;
+  public baseResources: StackResource[] | undefined;
 }
 export default CloudRunnerAWSTaskDef;
