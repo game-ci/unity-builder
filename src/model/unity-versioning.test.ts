9bcf76ecc88f9f9de61d75331f239a1d87e6b731
David Finol <davidmfinol@gmail.com>
2025-06-08 01:56:49 -0500

    Update Unity version

--- src/model/unity-versioning.test.ts
+++ src/model/unity-versioning.test.ts
@@ -7,9 +7,9 @@ describe('Unity Versioning', () => {
     });
 
     it('parses from ProjectVersion.txt', () => {
-      const projectVersionContents = `m_EditorVersion: 2021.3.4f1
-      m_EditorVersionWithRevision: 2021.3.4f1 (cb45f9cae8b7)`;
-      expect(UnityVersioning.parse(projectVersionContents)).toBe('2021.3.4f1');
+      const projectVersionContents = `m_EditorVersion: 2021.3.45f1
+      m_EditorVersionWithRevision: 2021.3.45f1 (cb45f9cae8b7)`;
+      expect(UnityVersioning.parse(projectVersionContents)).toBe('2021.3.45f1');
     });
 
     it('parses Unity 6000 and newer from ProjectVersion.txt', () => {
@@ -25,13 +25,13 @@ describe('Unity Versioning', () => {
     });
 
     it('reads from test-project', () => {
-      expect(UnityVersioning.read('./test-project')).toBe('2021.3.4f1');
+      expect(UnityVersioning.read('./test-project')).toBe('2021.3.45f1');
     });
   });
 
   describe('determineUnityVersion', () => {
     it('defaults to parsed version', () => {
-      expect(UnityVersioning.determineUnityVersion('./test-project', 'auto')).toBe('2021.3.4f1');
+      expect(UnityVersioning.determineUnityVersion('./test-project', 'auto')).toBe('2021.3.45f1');
     });
 
     it('use specified unityVersion', () => {
