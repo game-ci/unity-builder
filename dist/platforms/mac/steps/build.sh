f7f3f70c5760449a1d9f264093b94eb7be17d718
Ryo Oka <ryoka0418@gmail.com>
2025-09-30 18:55:14 +0900

    Support activeBuildProfile parameter (#738)
    * feat: add `-activeBuildProfile`
    
    * feat: descriptive error in case `-activeBuildProfile` is passed without actual value

--- dist/platforms/mac/steps/build.sh
+++ dist/platforms/mac/steps/build.sh
@@ -153,10 +153,11 @@ echo ""
   -password "$UNITY_PASSWORD" \
   -customBuildName "$BUILD_NAME" \
   -projectPath "$UNITY_PROJECT_PATH" \
-  -buildTarget "$BUILD_TARGET" \
+  $( [ -z "$BUILD_PROFILE" ] && echo "-buildTarget $BUILD_TARGET") \
   -customBuildTarget "$BUILD_TARGET" \
   -customBuildPath "$CUSTOM_BUILD_PATH" \
   -customBuildProfile "$BUILD_PROFILE" \
+  ${BUILD_PROFILE:+-activeBuildProfile} ${BUILD_PROFILE:+"$BUILD_PROFILE"} \
   -executeMethod "$BUILD_METHOD" \
   -buildVersion "$VERSION" \
   -androidVersionCode "$ANDROID_VERSION_CODE" \
